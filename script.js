e96ab900fba59cfd13d712f81326e131212fc67c
Pedro Augusto Costa Anjos <pedrooaugustoo130@gmail.com>
2023-03-20 14:37:10 -0300

    implementing tabbed component

--- script.js
+++ script.js
@@ -62,3 +62,31 @@ document
       document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
     }
   });
+
+///////////////////////////////////////
+// Tabbed Components
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+tabsContainer.addEventListener('click', function (event) {
+  event.preventDefault();
+  const clicked = event.target.closest('.operations__tab');
+
+  // Guard clause
+  if (!clicked) return;
+
+  // Remove active classes
+  tabs.forEach(tab => tab.classList.remove('operations__tab--active'));
+  tabsContent.forEach(content =>
+    content.classList.remove('operations__content--active')
+  );
+
+  // Active tab
+  clicked.classList.add('operations__tab--active');
+
+  // Activate content area
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
+});
